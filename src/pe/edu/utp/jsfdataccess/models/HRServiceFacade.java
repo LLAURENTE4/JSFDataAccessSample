3e9222832dda818adbf0fcd3c954cf556f6816d2
laurente <llaurente4@gmail.com>
2016-07-09 21:37:04 -0500

    Initial commit

---
+++ src/pe/edu/utp/jsfdataccess/models/HRServiceFacade.java
@@ -0,0 +1,53 @@
+package pe.edu.utp.jsfdataccess.models;
+
+import javax.sql.DataSource;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by GrupoUTP on 09/07/2016.
+ */
+
+@ManagedBean (name = "hrservicefacade", eager = true)
+@SessionScoped
+public class HRServiceFacade {
+    private Connection connection;
+
+
+    private Connection getConnection() {       return connection;    }
+
+    public HRServiceFacade(){
+
+        try {
+            InitialContext ctx = new InitialContext();
+                DataSource ds=(DataSource) ctx.lookup("jdbc/MySQLDataSource");
+                connection =ds.getConnection();
+            } catch (NamingException | SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+        private EmployeesEntity getEmployeesEntity(){
+        EmployeesEntity employeesEntity =new EmployeesEntity();
+        employeesEntity.setConnection(connection);
+        return employeesEntity;
+    }
+
+
+    //*retorna cantidad de empleados*//
+
+    public int getEmployeesCount(){
+        return  getEmployeesEntity().getEmployees().size();
+    }
+
+//*encapsulando *//
+
+    public List<Employee> getEmployees(){return  getEmployeesEntity().getEmployees();}
+
+
+}
